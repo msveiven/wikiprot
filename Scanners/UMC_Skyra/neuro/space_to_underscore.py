6122e95608a100c7c15f843863c69d26efc08ad3
msveiven <sveiven@email.arizona.edu>
2017-01-27 20:48:53 -0700

    First commit

---
+++ Scanners/UMC_Skyra/neuro/space_to_underscore.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import os
+
+for filename in os.listdir("."):
+    os.rename(filename, filename.replace(" ", "_"))
---
+++ copying_directories.py
@@ -0,0 +1,61 @@
+
+import sys
+import os
+from shutil import copyfile
+Path= os.getcwd().split('Scanners')
+PathLink = Path[0]
+
+
+
+newpath = os.getcwd()+'/apps'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+newpath = os.getcwd()+'/files'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+print(os.getcwd())
+newpath = os.getcwd()+'/files/theme'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+newpath = os.getcwd()+'/files/theme/images'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+newpath = os.getcwd()+'/uploads'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+newpath = os.getcwd()+'/uploads/9'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+newpath = os.getcwd()+'/uploads/9/9'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+newpath = os.getcwd()+'/uploads/9/9/0'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+newpath = os.getcwd()+'/uploads/9/9/0/7'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+newpath = os.getcwd()+'/uploads/9/9/0/7/99074558'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+copyfile(PathLink+'/htmlFiles/'+ 'index.html', 'index.html')
+copyfile(PathLink+'htmlFiles/'+ 'apps/audioPlayer2.swf', 'apps/audioPlayer2.swf')
+copyfile(PathLink+'htmlFiles/'+ 'files/main_style.css', 'files/main_style.css')
+copyfile(PathLink+'htmlFiles/'+ 'files/theme/custom.js', 'files/theme/custom.js')
+copyfile(PathLink+'htmlFiles/'+ 'files/theme/plugins.js', 'files/theme/plugins.js')
+copyfile(PathLink+'htmlFiles/'+ 'files/theme/manifest.json', 'files/theme/manifest.json')
+copyfile(PathLink+'htmlFiles/'+ 'files/theme/images/select-dropdown.png', 'files/theme/images/select-dropdown.png')
+copyfile(PathLink+'htmlFiles/'+ 'files/theme/images/default-bg.jpg', 'files/theme/images/default-bg.jpg')
+
+copyfile(PathLink+'htmlFiles/'+ 'uploads/9/9/0/7/99074558/umclogo.jpg', 'uploads/9/9/0/7/99074558/umclogo.jpg')
+copyfile(PathLink+'htmlFiles/'+ 'uploads/9/9/0/7/99074558/umclogo_orig.jpg', 'uploads/9/9/0/7/99074558/umclogo_orig.jpg')
